3dcf3ef81e36c1049d71d71712ce17a8617c906a
Stephen Fegan <sfegan@gmail.com>
2023-02-10 17:45:50 +0100

    First implementation of scripts to build author lists

---
+++ build_json_author_list.py
@@ -0,0 +1,231 @@
+import json
+import csv
+import unicodedata
+import re
+import pylatexenc.latexencode
+
+def asciify(x):
+    return unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8').strip()
+
+def htmlify(x):
+    return x.encode('ascii', 'xmlcharrefreplace').decode('utf8')
+
+def nbspify(x):
+    return re.sub('\s+', '&nbsp;', x)
+
+def author_asciified(firstname,lastname):
+    return asciify(re.sub('\s+',' ',firstname+' '+lastname))
+
+def author_unicode(firstname,lastname):
+    return re.sub('\s+',' ',firstname+' '+lastname)
+
+def author_latex(firstname,lastname):
+    author = re.sub('\s+','\u00A0',firstname+' '+lastname)
+    return pylatexenc.latexencode.unicode_to_latex(author)
+
+def author_html(firstname,lastname):
+    return nbspify(htmlify(firstname)+' '+htmlify(lastname))
+
+def author_latex(firstname,lastname):
+    author = re.sub('\s+','\u00A0',firstname+' '+lastname)
+    return pylatexenc.latexencode.unicode_to_latex(author)
+
+def short_place_key(short_place_name):
+    return re.sub(r'\W+', '', asciify(short_place_name))
+
+def load_alt_email_csv(filename):
+    alt_email = dict()
+    with open(filename) as fp:
+        csv_reader = csv.reader(fp)
+        for row in csv_reader:
+            if row[0]:
+                alt_email[row[0].lower()] = row[1].lower()
+    return alt_email
+
+def load_places_rows(rows):
+    places = dict()
+    short_place_keys = set()
+    for row in rows:
+        if row[0]:
+            if row[0] in places:
+                print('Warning : duplicate places id :',row[0])
+            key = short_place_key(row[1])
+            if(key in short_place_keys):
+                i = 2
+                while(key+str(i) in short_place_keys):
+                    i += 1
+                key = key+str(i)
+            short_place_keys.add(key)
+            places[row[0]] = [key, row[1], row[2], row[3]]
+        if '\u200B' in row[3]:
+            print("Warning, address contains character U200B :",row[3])
+    return places
+
+def load_places_csv(filename):
+    with open(filename) as fp:
+        csv_reader = csv.reader(fp)
+        return load_places_rows(csv_reader)
+
+def load_people_rows(rows):
+    people = dict()
+    for row in rows:
+        if row[0]:
+            if row[0] in people:
+                print('Warning : duplicate person id :',row[0])
+            people[row[0]] = [x.strip() for x in row[1:12]]
+    return people
+
+def load_people_csv(filename):
+    with open(filename) as fp:
+        fp.readline()
+        csv_reader = csv.reader(fp)
+        return load_people_rows(csv_reader)
+
+def load_signers_rows(rows):
+    signers = []
+    for row in rows:
+        signers.append([row[1], row[2], row[3], row[5]])
+    return signers
+
+def load_signers_csv(filename):
+    signers = []
+    with open(filename) as fp:
+        fp.readline()
+        csv_reader = csv.reader(fp)
+        return load_signers_rows(csv_reader)
+
+def format_author_name(firstname, lastname):
+    name = ''
+    firstname = firstname.strip()
+    lastname = lastname.strip()
+    name = lastname + ','
+    # Split the first name into names
+    firstnames = firstname.split()
+    for fn in firstnames:
+        if fn:        
+            if '-' in fn:
+                fn = '-'.join(map(lambda hfn: hfn[0] + '.', fn.split('-')))
+            else:
+                fn = fn[0] + '.'
+
+            name += ' ' + fn
+    return name
+
+alt_email = load_alt_email_csv('Copy of CTA_LMC_Optin (Responses) - Alternative email.csv')
+places = load_places_csv('Copy of CTA_LMC_Optin (Responses) - Affiliation addresses.csv')
+people = load_people_csv('Copy of CTA_LMC_Optin (Responses) - People.csv')
+signers = load_signers_csv('Copy of CTA_LMC_Optin (Responses) - Form responses 1.csv')
+
+main_emails = dict()
+cta_emails = dict()
+
+for pid in people:
+    p = people[pid]
+    main_email = p[2].lower()
+    cta_email = p[3].lower()
+
+    if(p[7] != format_author_name(p[1],p[0])):
+        print('Warning : signature name differs',p[7],"!=",format_author_name(p[1],p[0]))
+
+    if main_email:
+        if main_email in main_emails:
+            print('Warning : duplicate email address : %s (ids : %s and %s)'%(main_email,pid,main_emails[main_email]))
+        main_emails[main_email] = pid
+
+
+    if(cta_email):
+        if cta_email in cta_emails:
+            print('Warning : duplicate CTA email address : %s (ids : %s and %s)'%(cta_email,pid,cta_emails[cta_email]))
+        cta_emails[cta_email] = pid
+
+    if p[8].strip()=='':
+        print('Warning : author has no affiliation ',p[2])
+        if p[9] not in places:
+            print('Warning : author affiliation not found ',p[2])
+        if len(p)>9:
+            if p[9] and p[9] not in places:
+                print('Warning : author 2nd affiliation not found ',p[2])
+
+        if len(p)>10:
+            if p[10] and p[10] not in places:
+                print('Warning : author 3rd affiliation not found ',p[2])
+
+authors = set()
+for s in signers:
+    email = s[0].lower().strip()
+    if email in alt_email:
+        email = alt_email[email]
+    author_id = None
+    if email in main_emails:
+        author_id = main_emails[email]
+    elif email in cta_emails:
+        author_id = cta_emails[email]
+    if(author_id is None):
+        print('Warning : unknown author :',email)
+    else:
+        n1 = asciify(s[2]).lower().replace('-',' ')
+        n2 = asciify(people[author_id][0]).lower().replace('-',' ')
+            
+        if(n1 not in n2 and n2 not in n1):
+            print('Warning : author surname mismatch :',s[2],"!=",people[author_id][0])
+        if(author_id in authors):
+            print('Info : author already signed ',email)
+        else:  
+            authors.add(author_id)
+
+author_list = dict()
+affiliations_list = []
+author_affiliation_key = dict()
+for author_id in sorted(authors, key=lambda x: asciify(people[x][7]).lower()):
+    ai = asciify(people[author_id][7]).lower()
+    place_ids = []
+    affil_ids = []
+    affil_keys = []
+    for place_id in people[author_id][8:11]:
+        if(place_id):
+            if(place_id not in author_affiliation_key):
+                author_affiliation_key[place_id] = len(affiliations_list)
+                affiliations_list.append(dict(
+                    place_id          = place_id,
+                    place_key         = places[place_id][0],
+                    country           = places[place_id][2],
+                    address_asciified = asciify(places[place_id][3]),
+                    address_unicode   = places[place_id][3],
+                    address_html      = htmlify(places[place_id][3]),
+                    address_latex     = pylatexenc.latexencode.unicode_to_latex(places[place_id][3])
+                ))
+            place_ids.append(place_id)
+            affil_ids.append(author_affiliation_key[place_id])
+            affil_keys.append(places[place_id][0])
+            ai += ", %06d"%author_affiliation_key[place_id]
+    
+    lastname, firstname = people[author_id][7].split(',')
+    lastname = lastname.strip()
+    firstname = firstname .strip()
+
+    author_list[ai] = dict(
+        author_id        = author_id,
+        lastname         = people[author_id][0],
+        firstname        = people[author_id][1],
+        place_ids        = place_ids,
+        affil_ids        = affil_ids,
+        affil_keys       = affil_keys,
+        author_sortorder = asciify(people[author_id][7]).lower(),
+        author_asciified = author_asciified(firstname, lastname),
+        author_unicode   = author_unicode(firstname, lastname),
+        author_html      = author_html(firstname, lastname),
+        author_latex     = author_latex(firstname, lastname),
+    )
+
+
+author_affiliation_list = dict(
+    authors      = [author_list[x] for x in sorted(author_list)],
+    affiliations = affiliations_list
+)
+
+with open('authors.json','w') as fp:
+    json.dump(author_affiliation_list,fp,indent=4)
+
+print('')
+print("Number of authors:",len(author_list))
+print("Number of affiliations:",len(affiliations_list))
---
+++ render_authors_aanda.py
@@ -0,0 +1,29 @@
+import json
+
+with open('authors.json','r') as fp:
+    author_affiliation_list = json.load(fp)
+
+author_list = author_affiliation_list['authors']
+affiliations = author_affiliation_list['affiliations']
+
+with open('authors_aanda.tex','w') as fp:
+    print('\\documentclass[longauth,draft,bibyear]{aa}',file=fp)
+    print('\\begin{document}',file=fp)
+    print('\\title{Draft LMC paper author list}',file=fp)
+
+    for ia,author in enumerate(author_list):
+        inst = '\\inst{' + ','.join(['\\ref{'+x+'}' for x in author['affil_keys']]) + '}'
+        startswith = '\\author{' if ia==0 else '  \\and '        
+        print(startswith+author['author_latex']+inst,file=fp)
+    print('}',file=fp)
+            
+    for ia,affil in enumerate(affiliations):
+        startwith= '\\institute{' if ia==0 else '  \\and '
+        print(startwith+affil['address_latex']+'\\label{'+affil['place_key']+'}',file=fp)
+
+    print('}',file=fp)
+    print('\\maketitle',file=fp)
+    print('\\begin{thebibliography}{HELLO}',file=fp)
+    print('\\bibitem[1]{Fake reference to prompt A&A macro to emit instiutions}',file=fp)
+    print('\\end{thebibliography}',file=fp)
+    print('\\end{document}',file=fp)
